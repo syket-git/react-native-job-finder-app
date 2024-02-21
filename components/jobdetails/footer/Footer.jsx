510d12d6c0ed9d129fbb0fb758731f913a19a49f
syket <syketweb@gmail.com>
2024-02-21 23:52:27 +0600

    job details page design completed

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { Image, Linking, Text, TouchableOpacity, View } from "react-native";
 
-import styles from './footer.style'
+import { icons } from "../../../constants";
+import styles from "./footer.style";
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn}>
+        <Image
+          source={icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => Linking.openURL(url)}
+        style={styles.applyBtn}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
